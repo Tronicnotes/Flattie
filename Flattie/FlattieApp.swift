43c98650a5a50acb782a165f13e6ecaffca81c1b
Kurt Mohring <kurt.mohring@flybuys.com.au>
2023-06-04 17:03:54 +1000

    Initial Commit

---
+++ Flattie/FlattieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlattieApp.swift
+//  Flattie
+//
+//  Created by Kurt Mohring on 4/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlattieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
